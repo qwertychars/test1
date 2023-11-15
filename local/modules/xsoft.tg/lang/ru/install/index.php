d70121e5103fc59c195377435892f1bbd53c9fec
Yuriy <qwertychars@gmail.com>
2023-11-15 22:23:56 +0200

    git init

---
+++ local/modules/xsoft.tg/lang/ru/install/index.php
@@ -0,0 +1,6 @@
+<?
+$MESS["xsoft.tg_MODULE_NAME"] = "tg";
+$MESS["xsoft.tg_MODULE_DESC"] = "";
+$MESS["xsoft.tg_PARTNER_NAME"] = "xsoft";
+$MESS["xsoft.tg_PARTNER_URI"] = "bar1og.xyz";
+?>
---
+++ local/modules/xsoft.tg/lib/items.php
@@ -0,0 +1,15 @@
+<?php
+namespace Xsoft\Tg;
+
+use Bitrix\Main\ORM,
+	Bitrix\Main\Localization\Loc;
+
+Loc::loadMessages(__FILE__);
+
+class Items extends ORM\Data\DataManager
+{
+    public static function testttt()
+    {
+        echo '144423';die();
+    }
+}
